67a67788dcebcca7036c70a74c3be5fcbdff2def
worstkiller <vkbookworm@gmail.com>
2018-06-04 01:06:28 +0530

    feat: Added the parser

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -12,6 +13,10 @@ import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+const downLoadLink = "https://play.google.com/store/apps/details?id=";
+const PATTERN_THIRD = "<img\\s+(.*?)src=\\\"https:\\/\\/\\S.*?\\\"\\s+(.*?)alt=\\\"Cover art\\\"(.*?)>";
+const PATTERN_THIRD_URL = "https:\\/\\/\\S.*?(=?)(\\S.*?)\"\\s";
 
 /**
  * this styles the components 
@@ -48,39 +53,26 @@ const styles = theme => ({
     }
 });
 
+
 //item layout for list in card
 function SingleAppItem(props) {
     const { classes } = props;
-    const downLoadLink = "https://play.google.com/store/apps/details?id=";
-    console.log(props.value.attributes.component.nodeValue);
-
-    const componentObject = {
-        title: props.value.attributes.drawable.nodeValue,
-        packageName: getParsedAttribute(props.value.attributes.component.nodeValue),
-    };
-
-    //this returns a string parsed from component attribute
-    function getParsedAttribute(input) {
-        return input.substring(input.indexOf("{") + 1, input.indexOf("/"));
-    }
-
-    function processRequest(evt) {
-        console.log(evt.target);
-    }
+    console.log(props);
 
     return (
-        <ListItem button>
-            <CardMedia className={classes.cover} image={AppIcon} />
+        <ListItem >
+            <CardMedia id={props.value.packageName} className={classes.cover} image={AppIcon} />
             <div className={classes.details}>
                 <CardContent className={classes.content}>
-                    <Typography variant="headline">{componentObject.title}</Typography>
+                    <Typography variant="headline">{props.value.title}</Typography>
                     <Typography variant="subheading" color="textSecondary">
-                        {componentObject.packageName}
+                        {props.value.packageName}
                     </Typography>
                 </CardContent>
             </div>
             <CardActions className={classes.actions}>
-                <Button size="small" color="primary" onClick={processRequest}>
+                <Button id={props.value.packageName}
+                    size="small" color="primary" onClick={props.onDownload.bind(this, props.value)}>
                     Download
                     </Button>
             </CardActions>
@@ -89,23 +81,102 @@ function SingleAppItem(props) {
 }
 
 /**
- * this returns the base 64 image 
- * @param {img} img 
- */
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-
-/**
  * this returns a list items component
  */
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.setState = {urlUpdated:""};
+        this.processRequest = this.processRequest.bind(this);
+        this.urlExtractor = this.urlExtractor.bind(this);
+    }
+
+    options = {
+        method: 'get',
+        headers: {
+            "Allow-Control-Allow-Origin": "*",
+        },
+    }
+
+    //this returns a string parsed from component attribute
+    getParsedAttribute(input) {
+        return input.substring(input.indexOf("{") + 1, input.indexOf("/"));
+    }
+
+    //this returns a component object with title and package name
+    getComponentObject(singleItem) {
+        return {
+            title: singleItem.attributes.drawable.nodeValue,
+            packageName: this.getParsedAttribute(singleItem.attributes.component.nodeValue),
+        };
+    }
+
+    //this will load the url into card media
+    loadUrlIntoCardMedia(url, componentObject) {
+        document.getElementById(componentObject).src = url;
+        this.setState({urlUpdated:url});
+        console.log("image loaded into card view");
+    }
+
+    /**
+     * This will extract the url from string
+     * @param {String} myResponse 
+     */
+    urlExtractor(myResponse, evt) {
+        try {
+            const matchString = myResponse.match(PATTERN_THIRD);
+            const urlMatch = matchString[0].match(PATTERN_THIRD_URL);
+            var originalUrl = urlMatch[0];
+            if (originalUrl.includes("=")) {
+                originalUrl = originalUrl.substring(0, originalUrl.indexOf("=")) + "=s512";
+            } else if (originalUrl.includes(" ")) {
+                originalUrl = originalUrl.substring(0, originalUrl.indexOf(" ")) + "=s512";
+            }
+
+            this.loadUrlIntoCardMedia(originalUrl, evt.packageName);
+            console.log(urlMatch);
+            console.log(originalUrl);
+        } catch (error) {
+            alert("Downloading not supported for this icon");
+        }
+    }
+
+    /**
+        * This makes the http request and parse the url
+        * @param {evt} evt 
+        */
+    processRequest(evt) {
+        const finalUrl = downLoadLink + evt.packageName;
+        const urlExt = this.urlExtractor;
+        fetch(finalUrl, this.options)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (myResponse) {
+                if (myResponse != null || myResponse != undefined) {
+                    urlExt(myResponse, evt);
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            })
+        console.log(finalUrl);
+    }
+
+    /**
+     * this returns the base 64 image 
+     * @param {img} img 
+     */
+    getBase64Image(img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL("image/png");
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
 
     render() {
         var array = this.props.appFilter.appFilter;
@@ -114,13 +185,13 @@ class Home extends React.Component {
         if (array.getElementsByTagName("item") != null) {
             const itemArray = array.getElementsByTagName("item");
             var list = Array.from(itemArray);
-
             return (
                 <div>
                     <List className={classes.root}>
                         {list.map(singleItem => (
                             <Card key={singleItem.attributes.component.nodeValue}>
-                                <SingleAppItem {...this.props} value={singleItem} />
+                                <SingleAppItem {...this.props} onDownload={this.processRequest}
+                                    value={this.getComponentObject(singleItem)} />
                             </Card>
                         ))}
                     </List>
